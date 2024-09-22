76cc44a3be0c86178f5afd174510bf61d881a287
alessio-bonanno <168823083+alessio-bonanno@users.noreply.github.com>
2024-09-22 17:59:58 +0200

    state 'cursor' e 'conn' per 'db' richieste

---
+++ db/middleware.py
@@ -0,0 +1,24 @@
+from fastapi import Request
+import mysql.connector
+
+
+
+config = {
+    "host": "localhost",
+    "port": 3306,
+    "user": "root",
+    "autocommit": True, # conn.commit() ❌
+    "database": "mods_sneakers"
+}
+
+async def handle_db_conn(req: Request, call_next):
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor(dictionary=True)
+
+    req.state.cursor = cursor
+    req.state.conn = conn
+    response = await call_next(req)
+
+    cursor.close()
+    conn.close()
+    return response
