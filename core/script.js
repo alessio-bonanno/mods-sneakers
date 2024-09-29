4178b334202d4ac053e719bc64f31b30832d771f
alessio-bonanno <168823083+alessio-bonanno@users.noreply.github.com>
2024-09-29 14:32:46 +0200

    cambio struttura cartelle /js

--- core/script.js
+++ core/script.js
@@ -1,3 +1,26 @@
+class TemplateTransformation
+{
+    constructor(template)
+    {
+        this.template = template;
+    }
+
+    replace(options)
+    {
+        Object.keys(options).forEach(key => this.template = this.template.replace(`{${key}}`, options[key]));
+        return this;
+    }
+
+    create()
+    {
+        let temp = document.createElement("div");
+        temp.innerHTML = this.template;
+
+        return temp.childElementCount == 1 ? temp.firstElementChild : Array.from(temp.children);
+    }
+}
+
+
 class Toast
 {
     constructor(type, text, durationSeconds = 3)
